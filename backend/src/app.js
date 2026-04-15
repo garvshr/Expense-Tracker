82863b8708087a7aaebf4f3dcd8567cc672c31b0
garvshr <garvsharma40@gmail.com>
2026-04-15 19:45:23 +0530

    Configure for Vercel deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
